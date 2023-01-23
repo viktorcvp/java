a28fc63f25ccf8f38e76a3a3c1bb44bba50f5c94
“viktorcvp” <dffggg2013@yandex.by>
2023-01-23 12:00:10 +0300

    hom1

---
+++ src/java1/homework_java.java
@@ -0,0 +1,20 @@
+package java1;
+import java.util.Scanner;
+
+public class homework_java {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.print("Введите число : ");
+        int a = Scanner.nextInt();
+        Scanner.close();
+        System.out.println("Треугольное число = " + Triangle(a) + "\n");
+    }
+
+    public static int Triangle(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return Triangle(num - 1) + num;
+    }
+}
+
